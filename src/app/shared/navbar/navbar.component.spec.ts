995a2eeb36dcb3d7459ba5a24fd20023c0daf807
Mathis Vester <26164587+mathisvester@users.noreply.github.com>
2025-05-25 12:35:16 +0200

    test: migrate navbar test to testing-library

--- src/app/shared/navbar/navbar.component.spec.ts
+++ src/app/shared/navbar/navbar.component.spec.ts
@@ -1,35 +1,25 @@
-import { TestBed } from '@angular/core/testing';
 import { NavbarComponent } from './navbar.component';
 import { provideExperimentalZonelessChangeDetection } from '@angular/core';
+import { render, screen } from '@testing-library/angular';
 
 describe('NavbarComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NavbarComponent],
-      providers: [provideExperimentalZonelessChangeDetection()],
-    }).compileComponents();
-  });
-
-  it('should create', () => {
-    const fixture = TestBed.createComponent(NavbarComponent);
-    const component = fixture.componentInstance;
-    expect(component).toBeTruthy();
-  });
-
   it(`should render the 'branch from task' title`, async () => {
-    const fixture = TestBed.createComponent(NavbarComponent);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('span')?.textContent).toEqual(
-      'branch from task',
-    );
+    await render(NavbarComponent, {
+      providers: [provideExperimentalZonelessChangeDetection()],
+    });
+
+    expect(screen.getByText('branch from task')).toBeInTheDocument();
   });
 
   it('should render a link to GitHub respository', async () => {
-    const fixture = TestBed.createComponent(NavbarComponent);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('a')?.href).toEqual(
+    await render(NavbarComponent, {
+      providers: [provideExperimentalZonelessChangeDetection()],
+    });
+
+    expect(
+      screen.getByRole('link', { name: 'GitHub repository' }),
+    ).toHaveAttribute(
+      'href',
       'https://github.com/mathisvester/branch-from-task',
     );
   });
